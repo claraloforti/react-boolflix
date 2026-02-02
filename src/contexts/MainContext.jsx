e3566c96c2dbf4a9fee65ec4c20cfc4bcf2f413c
Clara Loforti <clara.loforti@gmail.com>
2026-02-03 00:54:11 +0100

    style

--- src/contexts/MainContext.jsx
+++ src/contexts/MainContext.jsx
@@ -5,7 +5,6 @@ const MainContext = createContext();
 
 const key = import.meta.env.VITE_API_KEY;
 
-console.log("Chiave API:", key);
 
 
 function MainProvider({ children }) {
@@ -15,27 +14,30 @@ function MainProvider({ children }) {
     // Variabile di stato per lista delle serie tv
     const [series, setSeries] = useState([]);
 
+    // Endpoints
+    const endpointMovies = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=`;
+    const endpointSeries = `https://api.themoviedb.org/3/search/tv?api_key=${key}&query=`;
+
+
+
 
     // Funzione che esegue chiamata verso endpointMovies
     function fetchMovies(query) {
-        if (!query) return; // non andare avanti se la query è vuota
-        const endpointMovies = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${encodeURIComponent(query)}`;
+        if (!query) return; // Non andare avanti se la query è vuota
 
-        axios.get(endpointMovies)
+        axios.get(endpointMovies + encodeURIComponent(query)) // converte la query in un formato adatto all'URL
             .then(response => {
                 console.log(response.data);
                 setMovies(response.data.results || []);
             })
-
             .catch(error => console.log("Errore nella richiesta"));
     }
 
     // Funzione che esegue chiamata verso endpointSeries
     function fetchSeries(query) {
         if (!query) return;
-        const endpointSeries = `https://api.themoviedb.org/3/search/tv?api_key=${key}&query=${encodeURIComponent(query)}`;
 
-        axios.get(endpointSeries)
+        axios.get(endpointSeries + encodeURIComponent(query))
             .then(response => setSeries(response.data.results))
             .catch(error => console.log("Errore nella richiesta"));
     }
